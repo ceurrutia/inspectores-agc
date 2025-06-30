0a8c05055d65e1d8ed1fdda20ebaf7c35cbb49ff
Cecilia urrutia <urrutia.cici@gmail.com>
2025-06-30 18:29:02 -0300

    agrego casos de test de login de admin y superadmin con sleniumWebDriver, Junit

--- src/test/java/com/agc/inspectores/InspectoresApplicationTests.java
+++ src/test/java/com/agc/inspectores/InspectoresApplicationTests.java
@@ -1,13 +1,8 @@
 package com.agc.inspectores;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InspectoresApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
---
+++ src/test/java/com/agc/inspectores/LoginTestBadCredentialLogin.java
@@ -0,0 +1,54 @@
+package com.agc.inspectores;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class LoginTestBadCredentialLogin {
+
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void setupDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    void loginWithValidCredentials_shouldGoToDashboard() {
+        driver.get("http://localhost:8080/auth/login");
+
+        WebElement usernameInput = driver.findElement(By.name("username"));
+        WebElement passwordInput = driver.findElement(By.name("password"));
+        WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+
+        usernameInput.sendKeys("usuario1");
+        passwordInput.sendKeys("contraseñaSegura123456");
+
+        loginButton.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        boolean urlChangedToDashboard = wait.until(ExpectedConditions.urlToBe("http://localhost:8080/auth/login?error"));
+
+        assertTrue(urlChangedToDashboard, "No se redirigió correctamente al dashboard de sueradmin después del login por credenciales incorectas.");
+
+    }
+}
